1cee55de570a756b8af84354ef0f2e8929b5dab7
Md Shamim <150214216+mdshamim125@users.noreply.github.com>
2024-04-25 17:47:00 +0600

    shamim

---
+++ src/components/app_components/DashboardComponent/Graph/PieChart.jsx
@@ -0,0 +1,40 @@
+import { Doughnut } from "react-chartjs-2";
+import { Chart as ChartJS, ArcElement, Tooltip, Legend } from "chart.js";
+ChartJS.register(ArcElement, Tooltip, Legend);
+
+ const data = {
+  labels: ["Red", "Blue", "Yellow", "Green", "Purple", "Orange"],
+  datasets: [
+    {
+      label: "# of Votes",
+      data: [12, 19, 3, 5, 2, 3],
+      backgroundColor: [
+        "rgba(255, 99, 132)",
+        "rgba(54, 162, 235)",
+        "rgba(255, 206, 86)",
+        "rgba(75, 192, 192)",
+        "rgba(153, 102, 255)",
+        "rgba(255, 159, 64)",
+      ],
+      borderColor: [
+        "rgba(255, 99, 132, 1)",
+        "rgba(54, 162, 235, 1)",
+        "rgba(255, 206, 86, 1)",
+        "rgba(75, 192, 192, 1)",
+        "rgba(153, 102, 255, 1)",
+        "rgba(255, 159, 64, 1)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+const PieChart = () => {
+    return (
+        <div>
+             <Doughnut data={data} />
+        </div>
+    );
+};
+
+export default PieChart;
--- src/routes/Page/Dashboard/DashboardContent.jsx
+++ src/routes/Page/Dashboard/DashboardContent.jsx
@@ -1,11 +1,23 @@
-import React from 'react';
+import Transaction from "@/components/app_components/DashboardComponent/Transaction";
+import PieChart from "@/components/app_components/DashboardComponent/Graph/PieChart";
+import LineChart from "@/components/app_components/DashboardComponent/Graph/LineChart";
 
-const DashboardContent = () => {
-    return (
-        <div>
-            Dashboard
+
+export default function DashboardContent() {
+  return (
+    <div className="flex min-h-screen w-full flex-col">
+      <div>
+        <Transaction></Transaction>
+      </div>
+
+      <div className="grid justify-center items-center col-span-1 gap-8 lg:grid-cols-2 xl:grid-cols-3 my-10">
+        <div className="xl:col-span-2">
+            <LineChart></LineChart>
         </div>
-    );
-};
-
-export default DashboardContent;
+        <div className="xl:col-span-1">
+          <PieChart></PieChart>
+        </div>
+      </div>
+    </div>
+  );
+}
